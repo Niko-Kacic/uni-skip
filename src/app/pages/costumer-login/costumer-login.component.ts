67b4e7800873a62e10f0f86126e48fb0c1af6ad6
NikoBoolean <niko.kacic@gmail.com>
2024-10-19 16:49:02 -0300

    Se crea la rama y primer formato del login

---
+++ src/app/pages/costumer-login/costumer-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-costumer-login',
+  templateUrl: './costumer-login.component.html',
+  styleUrl: './costumer-login.component.scss'
+})
+export class CostumerLoginComponent {
+
+}
